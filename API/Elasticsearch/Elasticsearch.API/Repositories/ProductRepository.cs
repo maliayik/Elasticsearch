ff53486036bbcec4ccc576f74ef393bb7bac83be
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-21 22:36:45 +0300

    ProductRepository initialize

---
+++ API/Elasticsearch/Elasticsearch.API/Repositories/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using Elasticsearch.API.Models;
+using Nest;
+
+namespace Elasticsearch.API.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly ElasticClient _elasticClient;
+
+        public ProductRepository(ElasticClient elasticClient)
+        {
+            _elasticClient = elasticClient;
+        }
+
+        public async Task<Product?> SaveAsync(Product newProduct)
+        {
+            newProduct.Created = DateTime.Now;
+
+            //product indexine yeni bir doküman eklemek için kullanılır.
+            var response = await _elasticClient.IndexAsync(newProduct,x=> x.Index("products"));
+
+            if (!response.IsValid) return null;
+
+            newProduct.Id = response.Id;
+
+            return newProduct;
+        }
+    }
+}
